adccb3c651470230f62bc2f1b6042069074fec0e
plapicola <p.lapicola@gmail.com>
2018-11-26 16:59:55 -0700

    adds correct and incorrect tests

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -16,4 +16,22 @@ class TurnTest < Minitest::Test
     turn = Turn.new("To find the holy grail", card)
     assert_equal turn.guess, "To find the holy grail"
   end
+
+  def test_it_has_a_card
+    card = Card.new("What is the meaning to life?", 42, :Philosiphy)
+    turn = Turn.new(42, card)
+    assert_equal turn.card, card
+  end
+
+  def test_guesses_can_be_correct
+    card = Card.new("What is the meaning to life?", 42, :Philosiphy)
+    turn = Turn.new(42, card)
+    assert turn.correct?
+  end
+
+  def test_guesses_can_be_incorrect
+    card = Card.new("What color is the sky?", "blue", :Science)
+    turn = Turn.new("Rainbow", card)
+    refute turn.correct?
+  end
 end
