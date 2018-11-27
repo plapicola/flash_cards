68420577b8c77a2f8b039c54f54bd33be0422926
plapicola <p.lapicola@gmail.com>
2018-11-26 17:30:02 -0700

    updates category search to always return array & adds test for cards of category not present in deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,4 +11,14 @@ class Deck
   def count
     return @cards.count
   end
+
+  def cards_in_category(category)
+    cards_found = []
+    cards.each do |card|
+      if card.category == category
+        cards_found << card
+      end
+    end
+    return cards_found
+  end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -29,4 +29,25 @@ class TestDeck < Minitest::Test
     deck = Deck.new(cards)
     assert_equal 3, deck.count
   end
+
+  def test_it_can_find_cards_by_category
+    card1 = Card.new("What color is the sky?", "blue", :Science)
+    card2 = Card.new("What is your quest?", "To seek the holy grail", :Movies)
+    card3 = Card.new("What is the meaning to life?", 42, :Philosiphy)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    assert_equal [card1], deck.cards_in_category(:Science)
+  end
+
+  def test_it_returns_no_cards_for_empty_categories
+    card1 = Card.new("What color is the sky?", "blue", :Science)
+    card2 = Card.new("What is your quest?", "To seek the holy grail", :Movies)
+    card3 = Card.new("What is the meaning to life?", 42, :Philosiphy)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    assert_equal [], deck.cards_in_category(:Politics)
+  end
 end
