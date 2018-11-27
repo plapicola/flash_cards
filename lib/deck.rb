3f0966120083a4abce8335e516a7914e76d2e172
plapicola <p.lapicola@gmail.com>
2018-11-26 17:20:16 -0700

    adds cards to deck, count of cards in deck, and related tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,7 +2,13 @@ require_relative 'card'
 
 class Deck
 
+  attr_reader :cards
+
   def initialize(cards = [])
     @cards = cards
+  end
+
+  def count
+    return @cards.count
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,11 +1,32 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require '../lib/deck'
+require '../lib/card'
 
 class TestDeck < Minitest::Test
 
   def test_it_exists
     deck = Deck.new()
     assert_instance_of Deck, deck
+  end
+
+  def test_it_is_made_of_cards
+    card1 = Card.new("What color is the sky?", "blue", :Science)
+    card2 = Card.new("What is your quest?", "To seek the holy grail", :Movies)
+    card3 = Card.new("What is the meaning to life?", 42, :Philosiphy)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    assert_equal deck.cards, cards
+  end
+
+  def test_it_counts_number_of_cards
+    card1 = Card.new("What color is the sky?", "blue", :Science)
+    card2 = Card.new("What is your quest?", "To seek the holy grail", :Movies)
+    card3 = Card.new("What is the meaning to life?", 42, :Philosiphy)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    assert_equal 3, deck.count
   end
 end
