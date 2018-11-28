5425b2305af4165cb6fbbdfacf918e6c4ab20366
plapicola <p.lapicola@gmail.com>
2018-11-28 15:06:03 -0700

    Adds card_generator and cards.txt

---
+++ lib/card_generator.rb
@@ -0,0 +1,18 @@
+require './card'
+
+class CardGenerator
+
+  def initialize(file)
+    @file = file
+  end
+
+  def cards
+    lines_in_file = IO.readlines(@file)
+    cards = {}
+    lines_in_file.each do |line|
+      line = line.chomp
+      card_properties = line.split(",")
+      cards << Card.new(card_properties[0], card_properties[1], card_properties[2].to_sym)
+    end
+  end
+end
--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -1,13 +1,15 @@
 require_relative 'round'
+require './card_generator'
 
 
-card1 = Card.new("What color is the sky?", "blue", :Science)
-card2 = Card.new("What is the top rated game of all time?", "Ocarina of Time", :Gaming)
-card3 = Card.new("What is the most played game in the world?", "League of Legends", :Gaming)
-card4 = Card.new("What was your companion in the original Pokemon game?", "Pikachu", :Gaming)
-card5 = Card.new("What system revitalized home consoles in North America?", "NES", :Gaming)
 
-deck = Deck.new([card1, card2, card3, card4, card5])
+# card1 = Card.new("What color is the sky?", "blue", :Science)
+# card2 = Card.new("What is the top rated game of all time?", "Ocarina of Time", :Gaming)
+# card3 = Card.new("What is the most played game in the world?", "League of Legends", :Gaming)
+# card4 = Card.new("What was your companion in the original Pokemon game?", "Pikachu", :Gaming)
+# card5 = Card.new("What system revitalized home consoles in North America?", "NES", :Gaming)
+#
+# deck = Deck.new([card1, card2, card3, card4, card5])
 round = Round.new(deck)
 
 round.start
