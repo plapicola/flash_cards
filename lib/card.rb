d14e8321f2b51b5dbb89a72f7a4ced9c2cfaa231
plapicola <p.lapicola@gmail.com>
2018-11-29 09:36:29 -0700

    adds card reference to turn card is present, refactors percentage methods

--- lib/card.rb
+++ lib/card.rb
@@ -1,10 +1,16 @@
 class Card
 
-  attr_reader :question, :answer, :category
+  attr_reader :question,
+              :answer,
+              :category
+
+  attr_accessor :turn
 
   def initialize(question, answer, category)
     @question = question
     @answer = answer
     @category = category
+    @turn = nil
   end
+
 end
